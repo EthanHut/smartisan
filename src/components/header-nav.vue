19678a9efbae925d69c13c2b0a41092efda79cbd
zjh <1429823270@qq.com>
2018-04-19 00:53:28 +0800

    初始化项目

---
+++ src/components/header-nav.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id='temp'>
+    <h1>{{headerMsg}}</h1>
+  </div>
+</template>
+<script>
+export default{
+  data () {
+    return {
+      headerMsg: 'hello world'
+    }
+  }
+}
+</script>
+<style scoped>
+  #temp{
+    height: 100%;
+    width: 100%;
+    text-align: center;
+  }
+  #temp h1{
+    color: #e9232c;
+    font-size: 30px;
+  }
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class='page'>
+    <div class="content">
+      我是首页
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<style scoped>
+  .page{
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    text-align: center;
+  }
+  .page .content{
+    width: 100%;
+    height: 100%;
+    font-size: 20px;
+    color: #000;
+    font-weight: bold;
+  }
+</style>
